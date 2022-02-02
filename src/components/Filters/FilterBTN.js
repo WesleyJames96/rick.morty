d41c4f62eb969172c470dad9e02be089657d5a74
Wesley Sarpong <wesley@longliveking.com>
2022-02-02 11:40:30 +0000

    updates

--- src/components/Filters/FilterBTN.js
+++ src/components/Filters/FilterBTN.js
@@ -1,25 +1,40 @@
 import React from "react";
 
-const FilterBTN = ({name, index, items}) => {
-  return (
-  <div>
-    <style jsx>
-    {`
-    .x:checked + label {
-        background-color: #0b5ed7;
-        color: #fff
-    }
-    input[type="radio"]{
-        display: none;
-    }
-    `}
-    </style>
-    <div className="form-check">
-        <input className="form-check-input x" type="radio" name={name} id={`${name}-${index}`}/>
-        <label className="btn btn-outline-primary" for={`${name}-${index}`}>{items}</label>
-    </div>
-  </div>
-  );
-};
+const FilterBTN = ({ input, task, updatePageNumber, index, name }) => {
+    return (
+      <div>
+        <style jsx>
+          {`
+            .x:checked + label {
+              background-color: #0b5ed7;
+              color: white;
+            }
+            input[type="radio"] {
+              display: none;
+            }
+          `}
+        </style>
+  
+        <div className="form-check">
+          <input
+            className="form-check-input x"
+            type="radio"
+            name={name}
+            id={`${name}-${index}`}
+          />
+          <label
+            onClick={(x) => {
+              task(input);
+              updatePageNumber(1);
+            }}
+            className="btn btn-outline-primary"
+            for={`${name}-${index}`}
+          >
+            {input}
+          </label>
+        </div>
+      </div>
+    );
+  };
 
 export default FilterBTN;
